2963ae8fcc852b05a015da1e79cbb09a227a09a4
Abdelrhman132006 <abdelrhman132006@gmail.com>
2026-04-15 17:00:36 +0200

    The Card structure is good now (animation left)

---
+++ src/components/BentoCard.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const BentoCard = ({ src, title, description }) => {
+    return (
+        <div className="relative size-full overflow-hidden">
+
+            {/* Background video */}
+            <video
+                src={src}
+                loop
+                muted
+                autoPlay
+                className="absolute inset-0 w-full h-full object-cover"
+            />
+
+            {/* Overlay content */}
+            <div className="relative z-10 flex flex-col px-6 py-7 gap-5">
+
+                <h2
+                    className="special-font font-zentry text-blue-50 text-6xl uppercase"
+                    dangerouslySetInnerHTML={{ __html: title || "" }}
+                />
+
+                <p
+                    className="max-w-xs special-font font-robert-regular text-blue-50 text-md"
+                    dangerouslySetInnerHTML={{ __html: description || "" }}
+                />
+
+            </div>
+        </div>
+    )
+}
+
+export default BentoCard
--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+
+import React from 'react';
+import BentoCard from './BentoCard';
 
 const Features = () => {
     return (
         // Changed max-h-screen to h-screen so the background actually shows up
-        <section className="max-h-screen bg-black relative overflow-hidden">
+        <section className="min-h-screen bg-black relative overflow-hidden">
             <div className="text-blue-50 font-circular text-lg mt-[120px] ml-[13vw] mb-[13vh] max-w-[80vw]">
                 {/* Wrapped title in a span or heading for better control */}
                 <span className="block mb-2 font-bold uppercase tracking-tighter">
@@ -18,8 +20,14 @@ const Features = () => {
                 </p>
 
             </div>
-            <div className="bg-black min-w-screen h-[1000px] border-t-1 border-white flex justify-center">
-                <div id="card1" className="bg-white mb-7 h-96 md:h-[65vh] w-[75vw] overflow-hidden rounded-md"></div>
+            <div className="bg-black min-w-screen h-auto flex justify-center">
+                <div id="card1" className="h-96 md:h-[65vh] w-[75vw] overflow-hidden rounded-lg mb-7 border-[1px] border-white/25">
+                    <BentoCard
+                        src="/videos/feature-1.mp4"
+                        title="radia<b>n</b>t"
+                        description="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
+                    />
+                </div>
             </div>
         </section>
     )
