ce3a568bf9c083295a8fe4e129e8fe1f3ba9043c
Abdelrhman132006 <abdelrhman132006@gmail.com>
2026-04-15 16:19:19 +0200

    Now working on features

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -3,22 +3,26 @@ import React from 'react'
 const Features = () => {
     return (
         // Changed max-h-screen to h-screen so the background actually shows up
-        <section className="max-h-screen bg-black flex relative overflow-hidden">
+        <section className="max-h-screen bg-black relative overflow-hidden">
             <div className="text-blue-50 font-circular text-lg mt-[120px] ml-[13vw] mb-[13vh] max-w-[80vw]">
                 {/* Wrapped title in a span or heading for better control */}
                 <span className="block mb-2 font-bold uppercase tracking-tighter">
                     Into Metagame Layer
                 </span>
-                <p>
+                <p className="opacity-60">
                     Immerse yourself in a rich and ever-expanding universe
                     <br />
                     where a vibrant array of products converge into an
                     <br />
                     interconnected overlay experience on your world.
                 </p>
+
+            </div>
+            <div className="bg-black min-w-screen h-[1000px] border-t-1 border-white">
+                hello
             </div>
         </section>
     )
 }
 
-export default Features
+export default Features;
