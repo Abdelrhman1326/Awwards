0d2f0906a6b8d7db03d434d283c39e8a1fcde6e3
Abdelrhman132006 <abdelrhman132006@gmail.com>
2026-04-18 02:18:47 +0200

    Now the basic structure of the Story extend section is responsive

--- src/components/StoryExtend.jsx
+++ src/components/StoryExtend.jsx
@@ -5,10 +5,10 @@ const StoryExtend = () => {
                 {/* Added flex flex-col items-center here */}
                 <div className="bg-black min-w-full h-[500px] p-25 flex flex-col items-center">
                     <p className="uppercase text-xs text-blue-50 text-center mb-6">Join Zentry</p>
-                    <h1 className="text-blue-50 text-center special-font uppercase font-zentry text-8xl mb-10">
+                    <h1 className="text-blue-50 text-center special-font uppercase font-zentry text-4xl sm:text-6xl lg:text-7xl xl:text-8xl mb-10">
                         Let's build the <br /> new era of g<b>a</b>ming t<b>o</b>gether.
                     </h1>
-                    <button className="font-robert-normal text-sm bg-blue-50 text-black/90 px-6 py-3 rounded-full">
+                    <button className="font-robert-normal text-xs min-w-[120px] md:text-sm bg-blue-50 text-black/90 px-6 py-3 rounded-full">
                         contact us
                     </button>
                 </div>
